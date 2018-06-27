0da96a1dd4e7883bed705b13a0c3ed2f0057879a
Pranav <pranavlarry111@gmail.com>
2018-06-27 21:26:08 +0530

    node js server study

---
+++ server.js
@@ -0,0 +1,31 @@
+const express=require('express');
+const hbs=require('hbs');
+const fs=require('fs');
+var app=express();
+
+hbs.registerPartials(__dirname+'/views/partials')
+app.set('view engine','hbs');
+hbs.registerHelper('year',()=>{
+  return new Date().getFullYear();
+});
+
+app.use((req,res,next)=>{
+  var now=new Date().toString();
+  var log=`${now}: ${req.method} ${req.url}`;
+  fs.appendFile('server.log',log+'\n');
+  console.log(log);
+  next();
+});
+app.use(express.static(__dirname + '/public'));
+app.get('/',(req,res)=>{
+  res.render('home.hbs',{
+    pagetitle:'Home Page'
+  });
+});
+
+app.get('/about',(req,res)=>{
+  res.render('about.hbs',{
+    pagetitle:'About Page'
+  });
+});
+app.listen(4000);
